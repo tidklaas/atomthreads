b541e29bbabd68207a642a7af1331dca949cfbdb
Tido Klaassen <tido@4gh.eu>
2016-04-07 19:46:46 +0200

    Add Support for NL6621
    Add code for board setup, serial console and linker script for
    Nufront's NL6621-Y1 board.
    
    Switch libopencm3 to https://github.com/tidklaas/libopencm3.git on
    branch nl6621

---
+++ ports/cortex-m/boards/nl6621-y1/board_setup.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2016, Tido Klaassen. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. No personal names or organizations' names associated with the
+ *    Atomthreads project may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE ATOMTHREADS PROJECT AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdbool.h>
+
+#include <libopencm3/nufront/syscfg.h>
+#include <libopencm3/nufront/uart.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/cm3/cortex.h>
+#include <libopencm3/cm3/nvic.h>
+
+#include "atomport.h"
+
+/**
+ * initialise and start SysTick counter. This will trigger the
+ * sys_tick_handler() periodically once interrupts have been enabled
+ * by archFirstThreadRestore()
+ */
+static void systick_setup(void)
+{
+    systick_set_frequency(SYSTEM_TICKS_PER_SEC, 40000000);
+
+    systick_interrupt_enable();
+
+    systick_counter_enable();
+}
+
+void test_led_toggle(void)
+{
+    /* dummy, LED is conected to UART TX */
+    uart_putc('.');
+}
+
+/**
+ * Callback from your main program to set up the board's hardware before
+ * the kernel is started.
+ */
+int board_setup(void)
+{
+    /* Disable interrupts. This makes sure that the sys_tick_handler will
+     * not be called before the first thread has been started.
+     * Interrupts will be enabled by archFirstThreadRestore().
+     */
+    cm_mask_interrupts(true);
+
+    /* configure system clock, user LED and UART */
+    syscfg_clk_init(cpuclk_160, apbclk_40, gpiobw_20);
+    uart_init(19200);
+
+    /* initialise SysTick counter */
+    systick_setup();
+
+    /* Set exception priority levels. Make PendSv the lowest priority and
+     * SysTick the second to lowest
+     */
+    nvic_set_priority(NVIC_PENDSV_IRQ, 0xFF);
+    nvic_set_priority(NVIC_SYSTICK_IRQ, 0xFE);
+
+    return 0;
+}
+
